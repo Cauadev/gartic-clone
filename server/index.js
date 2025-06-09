e3ee61998ac1108b10352a57e5f0f168f676403c
CAUΛ <cauadev@gmail.com>
2025-06-09 20:57:02 -0300

    Coisas

--- server/index.js
+++ server/index.js
@@ -5,7 +5,30 @@ const { Server } = require('socket.io');
 
 const app = express();
 
-// 🔓 Libera CORS para o frontend
+let hostId = null; 
+
+let players = new Array()
+let GAME_STATE = "WAITING" //'waiting', 'writing', 'drawing', 'finished'
+const nicknames = [
+  'Zorpax', 'Nuklodon', 'XxC4rbonxX', 'BlipZonk', 'Kraxxor',
+  'V1rusTTV', 'Lun4tik', '0xDeadMeat', 'Snorblo', 'YeloToes',
+  'W33dDuck', 'Flipzard', 'B1tzkrieg', 'Krumpz', 'HexaTuna',
+  'Ch0koBlast', 'DripKing69', 'PwnSauce', 'SkibidiZap', 'Voidling'
+];
+
+function getRandomNick() {
+  if (players.length >= nicknames.length) {
+    throw new Error('Todos os nicks já foram usados.');
+  }
+
+  const index = Math.floor(Math.random() * nicknames.length);
+  const selected = nicknames[index];
+
+  return players.some(p => p.nick === selected)
+    ? getRandomNick()
+    : selected;
+}
+
 app.use(cors({
   origin: 'http://localhost:5173', // frontend
   methods: ['GET', 'POST']
@@ -13,7 +36,6 @@ app.use(cors({
 
 const server = http.createServer(app);
 
-// ✅ Libera CORS para o Socket.IO
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:5173', // frontend
@@ -22,15 +44,58 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-  console.log('New client connected');
 
-  socket.on('message', (message) => {
-    console.log('Message received:', message);
-    io.emit('message', message); // Broadcast para todos
-  });
+  socket.on('join', (uuid) => {
+    console.log("joined")
+    if(!hostId) hostId = socket.id
+
+    players.push({
+      id: socket.id,
+      nick: getRandomNick(),
+      isHost: socket.id == hostId,
+      uuid
+    });
+
+    io.emit('updated-players', players)
+  })
+
+  socket.on('change-nick', (nick) => {
+    if(!!nick && nick.length > 0){
+      players = players.map(player => {
+        if(player.id == socket.id){
+          player.nick = nick
+          console.log(player.nick)
+        }
+        return player
+      })
+      io.emit('updated-players', players)
+    }
+  })
+
+  // socket.on('message', (message) => {
+  //   console.log(hostId)
+  //   console.log('Message received:', message);
+  //   io.emit('message', message); // Broadcast para todos
+  // });
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
+
+    console.log("disconnected")
+
+    //remove player
+    const index = players.findIndex(p => p.id === socket.id);
+    if (index !== -1) players.splice(index, 1);
+
+
+    //se o host sair seta outro como host
+    if(socket.id === hostId && players.length > 0){
+      hostId = players[0].id
+      players[0].isHost = true;
+    }else{
+      hostId = null
+    }
+
+    io.emit('updated-players', players)
   });
 });
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,34 +1,114 @@
 <template>
- <div>
-   <input type="text" v-model="message" placeholder="mensagem">
-   <Button label="Send" @click="sendMessage"/>
+ <!-- <div>
+  <div style="max-width: 220px;">
+    <Textarea rows="3" placeholder="mensagem" size="large"/><br>
+    <Button fluid label="Send" @click="sendMessage"/>
+  </div>
 
     <div class="messages">
       <div v-for="(message, index) in messages" :key="index" class="message">
         {{ message }}
       </div>
     </div>
+ </div> -->
+
+ <div class="options">
+  <Slider id="brush_size" v-model="BRUSH_SIZE" :min="1" :max="100" class="w-56" />
+  <ColorPicker v-model="BRUSH_COLOR" inline/>
+  <Button @click="gerarImagem" fluid severity="info" label="gera imagem"/>
+  <Button @click="baixarImagem" fluid severity="help" label="baixar imagem"/>
  </div>
 
- <canvas id="draw_board" width="600" height="800" style="border: 1px solid #000;"></canvas>
+ <div>
+  <center><span v-if="player?.isHost">HOST</span></center>
+   <InputGroup>
+    <InputText v-model="newNick" placeholder="novo nick"/>
+    <Button @click="updateNick" label="atualizar nick"/>
+   </InputGroup>
+  <br>
+  <ul>
+    <li v-for="(player, index) in players" :key="index">{{ player.nick }}</li>
+  </ul>
+ </div>
+
+ <div style="display: flex; justify-content: center; margin-top: 50px;">
+   <canvas @mousedown="drawing = true" @mouseup="stopDraw" @mousemove="draw($event)" id="draw_board" width="600" height="800" style="border: 1px solid #000;"></canvas>
+ </div>
+
+ <div id="image-container">
+
+ </div>
+
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import Button from "primevue/button"
+import InputText from "primevue/inputtext"
 import socket from '../../service/socket';
+import { ColorPicker, InputGroup, Slider, Textarea } from 'primevue';
 
 const message = ref<string>("");
 const messages = ref<string[]>([])
+const drawing = ref(false);
+const canvas = ref<HTMLCanvasElement | null>();
+const ctx = ref<CanvasRenderingContext2D | null>();
+const BRUSH_SIZE = ref(3);
+const BRUSH_COLOR = ref('BLACK');
+const image = ref<string | null>(null);
+const players = ref<Player[]>([])
+const player = ref<Player>()
+const newNick = ref('');
+const uuid = ref(crypto.randomUUID())
 
 const sendMessage = () => {
   socket.emit('message', message.value)
 }
 
+const updateNick = () => {
+  if(newNick.value && newNick.value.length > 0){
+    socket.emit('change-nick', newNick.value)
+  }
+}
+
+interface Player {
+  id: string,
+  nick: string,
+  isHost: boolean,
+  uuid: string
+}
+
 onMounted(() => {
-  socket.on('message', (msg) => {
-    messages.value.push(msg);
-  });
+  // socket.on('message', (data) => {
+  //   // messages.value.push(data);
+
+  //   const blob = new Blob([data], { type: 'image/png' });
+
+  //   const img = new Image();
+  //   img.src = URL.createObjectURL(blob);
+  //   img.alt = 'Imagem recebida';
+  //   img.className = 'imagem-recebida'; // Você pode estilizar com CSS
+
+  //   // Insere no DOM, por exemplo em um container
+  //   const container = document.getElementById('image-container');
+  //   if (container) {
+  //     container.appendChild(img);
+  //   }
+
+  //   // Libera memória depois que a imagem carregar
+  //   img.onload = () => {
+  //     URL.revokeObjectURL(img.src);
+  //   };
+  // });
+
+  socket.emit('join', uuid.value)
+
+
+
+  socket.on('updated-players', (onlinePlayers: Player[]) => {
+    players.value = onlinePlayers;
+    player.value = onlinePlayers.find(p => p.uuid === uuid.value)
+  })
 })
 
 
@@ -39,34 +119,79 @@ const moveFromTo = (context: CanvasRenderingContext2D, from: {x: number, y: numb
     context.strokeStyle = 'black'
     context.lineWidth = size
     context.stroke()
+    context.beginPath()
 }
 
+const stopDraw = () => {
+  drawing.value = false;
+  ctx.value?.beginPath();
+}
+
+const draw = (event: MouseEvent) => {
+  if(!!canvas.value && !!ctx.value){
+    if(!drawing.value) return;
+
+    const rect = canvas.value.getBoundingClientRect();
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+
+    ctx.value.lineWidth = BRUSH_SIZE.value;
+    ctx.value.lineCap = 'round'
+    ctx.value.strokeStyle = `#${BRUSH_COLOR.value}`
+
+    ctx.value.lineTo(x, y)
+    ctx.value.stroke()
+    ctx.value.beginPath()
+    ctx.value.moveTo(x, y)
+  }
+}
+
+const gerarImagem = () => {
+  if (!canvas.value) return;
+  canvas.value.toBlob((blob) => {
+    if(blob){
+      blob.arrayBuffer().then((buffer) => {
+         socket.emit('message', buffer)
+      })
+    }
+  }, 'image/png', 0.8);
+};
+
+const baixarImagem = () => {
+  if (!canvas.value) return;
+  const link = document.createElement('a');
+  link.href = canvas.value.toDataURL('image/png');
+  link.download = 'meu-desenho.png';
+  link.click();
+};
 
 onMounted(() => {
-  const canvas = document.getElementById('draw_board') as HTMLCanvasElement;
-  const ctx = canvas.getContext('2d')
+
+  canvas.value = document.getElementById('draw_board') as HTMLCanvasElement
+  ctx.value = canvas.value.getContext('2d')
+
   
   console.log(ctx)
 
-  if(ctx){
-    ctx.fillStyle = 'blue'
-    // x, y , largura, altura
-    ctx.fillRect(50, 50, 100, 100)
+  if(ctx.value){
+    // ctx.value.fillStyle = 'blue'
+    // // x, y , largura, altura
+    // ctx.value.fillRect(50, 50, 100, 100)
 
 
-    //Retangulo stroked
-    ctx.strokeStyle = 'red'
-    ctx.strokeRect(200, 50, 100, 100);
+    // //Retangulo stroked
+    // ctx.value.strokeStyle = 'red'
+    // ctx.value.strokeRect(200, 50, 100, 100);
 
 
-    ctx.beginPath();
-    ctx.arc(180, 200, 40, 0, 2 * Math.PI)
-    ctx.fillStyle = 'green'
-    ctx.fill()
-    ctx.stroke();
+    // ctx.value.beginPath();
+    // ctx.value.arc(180, 200, 40, 0, 2 * Math.PI)
+    // ctx.value.fillStyle = 'green'
+    // ctx.value.fill()
+    // ctx.value.stroke();
 
-    //LINHAS
-    moveFromTo(ctx, { x: 10, y: 350}, { x: 500, y: 400}, 1)
+    // //LINHAS
+    // moveFromTo(ctx.value, { x: 10, y: 350}, { x: 500, y: 400}, 1)
   }
   
 })
@@ -80,5 +205,15 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.options{
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
 
+#brush_size{
+  width: 15rem;
+}
 </style>
